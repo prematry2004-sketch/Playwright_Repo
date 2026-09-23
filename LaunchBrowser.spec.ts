3dad415250a0ce7ac4673b99f6a3120c9ee4c14e
prematry2004-sketch <prematry2004@gmail.com>
2026-09-24 09:33:58 +1000

    Add files via upload
    Tried Launch browser

---
+++ LaunchBrowser.spec.ts
@@ -0,0 +1,13 @@
+import { chromium, test } from "@playwright/test";
+
+test("To launch a browser", async () => {
+
+const browserInstance = await chromium.launch({ headless: false, channel: "chrome" });
+
+const browserContext = await browserInstance.newContext();
+
+const page = await browserContext.newPage();
+
+await page.goto("https://grannyflatunder25k.com.au/");
+
+});
